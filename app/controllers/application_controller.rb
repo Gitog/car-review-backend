af43fcaa32203ab31a39766511f85015807169d5
James <jamesnjorio@gmail.com>
2022-11-08 14:07:08 +0300

    migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,20 @@ class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
 
     get '/cars' do
+       cars = Car.all
+       cars.to_json
     end
+
+    get '/users' do
+        users = User.all
+        users.to_json
+     end
+
+     get '/reviews' do
+        reviews = Review.all
+        reviews.to_json
+     end
+ 
+ 
+
 end
